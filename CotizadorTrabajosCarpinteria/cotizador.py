a814664de63bea1f6dcdbc7f0b0ff317b27c0d0b
Alejandro <dev.alejandrolescano@gmail.com>
2022-10-28 15:47:33 -0300

    Agrega interface

--- CotizadorTrabajosCarpinteria/cotizador.py
+++ CotizadorTrabajosCarpinteria/cotizador.py
@@ -1,10 +1,24 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog
 from PyQt5 import uic
 
 class Vista(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("CotizadorTrabajosCarpinteria/cotizador.ui", self)
+        self.btnAgregar.clicked.connect(self.agregar_item)
+        self.btnEditar.clicked.connect(self.editar_item)
+        self.btnEliminar.clicked.connect(self.eliminar_item)
+        self.rbCepillado.toggled.connect(self.agregar_item)
+        self.rbFlete.toggled.connect(self.agregar_item)
+
+    def agregar_item(self):
+        pass
+
+    def editar_item(self):
+        pass
+
+    def eliminar_item(self):
+        pass
 
 
 aplicacion = QApplication([])
